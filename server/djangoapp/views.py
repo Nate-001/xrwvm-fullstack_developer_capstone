44ef2a01711b1d5014352db730c3a60cdf43ce9c
Nate-001 <nathanieldross1@gmail.com>
2024-05-09 15:11:32 -0400

    Changes to login, logout and register.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -51,29 +51,29 @@ def registration(request):
 
     data = json.loads(request.body)
     username = data['userName']
-    passowrd = data['password']
+    password = data['password']
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    email_exists = False
+    email_exist = False
     try:
-        # Check if user already exsits
+        # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except:
         # If not, simply log this is a new user
-        logger.debug("{} is a new user".format(username))
-    
+        logger.debug("{} is new user".format(username))
+
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=passowrd, email=email)
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
         # Login the user and redirect to list page
-        login(request,user)
+        login(request, user)
         data = {"userName":username,"status":"Authenticated"}
         return JsonResponse(data)
-    else:
+    else :
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
         
